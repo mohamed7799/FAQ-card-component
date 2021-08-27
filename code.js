baf3a9ecfe5e89a84aae91aadb84e504766911a6
mohmed <mohmedamged9@gmail.com>
2021-08-27 14:15:19 +0200

    show the answers by clicking on questions not arrows and remove the box outside the container

--- code.js
+++ code.js
@@ -4,41 +4,42 @@ let faq_section = document.getElementById("faq-section-js");
 
 let arrows = document.getElementsByClassName("arrow-js");
 
-let answers = document.getElementsByClassName("answer");
+let items = document.querySelectorAll(".items");
 
-let state = 0;
+let answers = document.getElementsByClassName("answer");
 
 // fun
 
 let check = (item, _class) => item.classList.contains(_class);
 let add = (item, _class) => item.classList.add(_class);
 let remove = (item, _class) => item.classList.remove(_class);
-let toggle = (item, _class) => check(item, _class) ? remove(item, _class) : add(item, _class);
+let toggle = (item, _class) =>
+  check(item, _class) ? remove(item, _class) : add(item, _class);
 
 let reset = () => {
-    for (const arrow of arrows) {
-        remove(arrow, "arrow-up");
-    }
-    for (const answer of answers) {
-        add(answer, "hidden");
-    }
-
-}
+  for (const arrow of arrows) {
+    remove(arrow, "arrow-up");
+  }
+  for (const answer of answers) {
+    add(answer, "hidden");
+  }
+};
 
 // event listner
-window.addEventListener('click', function (e) {
-    if (faq_section.contains(e.target)) {
-        // click inside the container
-        let arrow = e.target;
-        reset();
-        if (check(arrow, "arrow-js")) {
-            toggle(arrow, "arrow-up");
-            let hiddenTarget = arrow.parentElement.nextElementSibling;
-            toggle(hiddenTarget, "hidden");
+window.addEventListener("click", function (e) {
+  if (faq_section.contains(e.target)) {
+    // click inside the container
+    let item = e.target;
+    let arrow = item.firstElementChild;
 
-        }
-    } else {
-        //click outside the container
-        reset();
+    reset();
+    if (check(item, "quest")) {
+      toggle(arrow, "arrow-up");
+      let hiddenTarget = item.nextElementSibling;
+      toggle(hiddenTarget, "hidden");
     }
+  } else {
+    //click outside the container
+    reset();
+  }
 });
